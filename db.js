2ba91207bb3d816cf0c20c72c6852cc39a6cab58
Juan <jguardado85@gmail.com>
2017-02-16 08:30:10 -0800

    basic api for store

---
+++ db.js
@@ -0,0 +1,30 @@
+var promise = require('bluebird');
+
+var options = {
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/sonris_dietetics'
+var db = pgp(connectionString);
+
+function getAllContent(req, res, next) {
+  console.log("getAllContent being called");
+  db.any('select * from product_feed')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL content'
+        });
+    })
+    .catch(function (err) {
+      console.log("theres a problem");
+      return next(err);
+    });
+}
+
+module.exports = {
+  getAllContent: getAllContent,
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var store = require("./routes")
+var db = require('./db.js');
+
+var port = process.env.PORT || '3000';
+
+app.get('/', function (req, res) {
+  res.send("Thank you for visting our API")
+});
+
+app.get('/api/store', db.getAllContent);
+
+app.listen(port, function () {
+  console.log(' Example app listening on port 3000!');
+});
