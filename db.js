9a707d8ef0a6a6d931552e50f8347ca18bfe722d
Juan <jguardado85@gmail.com>
2017-02-17 09:21:02 -0800

    changed postgres url

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/sonris_dietetics'
+// var connectionString = 'postgres://localhost:5432/sonris_dietetics';
+var connectionString = "postgres://idhzyrhlxzmoln:11c106d837f2df7b337789ca18bf15a584e3bce8de63256cd69cf281b18275c1@ec2-54-235-240-92.compute-1.amazonaws.com:5432/d22d8rba2gvl0t"
 var db = pgp(connectionString);
 
 console.log("db: ", db);
