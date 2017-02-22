00048373b669c0f9efad6a3815d7a778c4607bd7
Juan <jguardado85@gmail.com>
2017-02-21 21:13:14 -0800

    serving static csv file

--- db.js
+++ db.js
@@ -1,33 +1,42 @@
 var promise = require('bluebird');
+var json2csv = require('json2csv');
 
 var options = {
   promiseLib: promise
 };
+var fields = ['field1', 'field2', 'field3'];
 
 var pgp = require('pg-promise')(options);
 // var connectionString = 'postgres://localhost:5432/sonris_dietetics';
 var connectionString = "postgres://idhzyrhlxzmoln:11c106d837f2df7b337789ca18bf15a584e3bce8de63256cd69cf281b18275c1@ec2-54-235-240-92.compute-1.amazonaws.com:5432/d22d8rba2gvl0t"
 var db = pgp(connectionString);
 
-console.log("db: ", db);
+// console.log("db: ", db);
 
 function getAllContent(req, res, next) {
-  console.log("getAllContent being called");
+  // console.log("getAllContent being called");
   db.any('select * from product_feed')
     .then(function (data) {
       res.status(200)
-        .json({
-          status: 'success',
+        .json2csv({
           data: data,
-          message: 'Retrieved ALL content'
+          fields: fields
         });
     })
     .catch(function (err) {
-      console.log("theres a problem");
+      // console.log("theres a problem");
       return next(err);
     });
+};
+
+function sendLocalCSV (req, res, next) {
+  // console.log("sneding csv");
+  res.attachment(__dirname + 'product-feed.csv');
+  res.status(200).send(data);
 }
+
 
 module.exports = {
   getAllContent: getAllContent,
+  sendLocalCSV: sendLocalCSV
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var csv = require('express-csv')
 var db = require('./db.js');
 
 var port = process.env.PORT || '3000';
@@ -10,7 +11,11 @@ app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/api/store', db.getAllContent);
+// app.get('/api/store', db.getAllContent)
+
+app.get('/api/store', function(req, res) {
+  res.sendFile(__dirname + "/product-feed.csv")
+});
 
 app.listen(port, function () {
   console.log(' Example app listening on port 3000!');
