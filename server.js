d16570ae697db4c1cc599176d72b493afef34ecc
Juan <jguardado85@gmail.com>
2017-02-25 08:05:53 -0800

    adding images for api and building routes for static renders

--- server.js
+++ server.js
@@ -7,15 +7,75 @@ var db = require('./db.js');
 var port = process.env.PORT || '3000';
 
 
-app.get('/',function(req,res){
+app.get('/',function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-// app.get('/api/store', db.getAllContent)
 
 app.get('/api/store', function(req, res) {
   res.sendFile(__dirname + "/product-feed.csv")
 });
+
+// app.get('/api/store', db.getAllContent)
+
+/* [Routes for images] */
+
+app.get('/api/pics/hydroxycut', function(req, res) {
+  res.sendFile(__dirname + "/assests/products/hydroxycut.jpg")
+});
+
+app.get('/api/pics/select-vegan-protien', function(req, res) {
+  res.sendFile(__dirname + "/assests/products/PES-select-vegan-protien.jpg")
+})
+
+app.get('/api/pics/pandy-protein-candy-salty-kettlebells', function(req, res) {
+  res.sendFile(__dirname + "/assests/products/pandy-protein-candy-salty-kettlebells.jpg")
+});
+
+app.get('/api/pics/muscletech-100-platinum-casein', function(req, res) {
+  res.sendFile(__dirname + "/assests/products/muscletech-100-platinum-casein.jpg")
+});
+
+app.get("/api/pics/muscletech-100-premium-whey-protein", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/muscletech-100-premium-whey-protein.jpg")
+})
+
+app.get("/api/pics/nitrotech-performance-series-2lbs", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/nitrotech-performance-series-2lbs.jpg")
+})
+
+app.get("/api/pics/pandy-protein-candy-soda-shaker", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/pandy-protein-candy-soda-shaker")
+})
+
+app.get("/api/pics/pandy-protein-candy-sour-plates", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/pandy-protein-candy-sour-plates")
+})
+
+app.get("/api/pics/inspired-100-plant-based-protein", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/inspired-100-plant-based-protein")
+})
+
+app.get("/api/pics/musclepharm-combat-powder", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/musclepharm-combat-powder")
+})
+
+app.get("/api/pics/creatine", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/creatine.jpg")
+})
+
+app.get("/api/pics/bpi-best-bcaa", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/bpi-best-bcaa.jpg")
+})
+
+
+// SAMPLE
+app.get("/api/pics/", function(req, res) {
+  res.sendFile(__dirname + "/assests/products/")
+})
+
+
+
+/* Listening PORT */
 
 app.listen(port, function () {
   console.log(' Example app listening on port 3000!');
